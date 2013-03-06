b52c9a2ea4c1463f66a9615b9e1237a5efcbcb05
sosandstrom <os@goldengekko.com>
2013-03-06 11:07:39 +0700

    Added queryByProviderIdProviderUserId()

--- src/main/java/com/wadpam/oauth2/service/ConnectionService.java
+++ src/main/java/com/wadpam/oauth2/service/ConnectionService.java
@@ -14,6 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author sosandstrom
  */
 public class ConnectionService extends MardaoCrudService<DConnection, String, DConnectionDao> {
+    
+    public Iterable<DConnection> queryByProviderIdProviderUserId(String providerId, String providerUserId) {
+        return dao.queryByProviderIdProviderUserId(providerId, providerUserId);
+    }
 
     @Autowired
     public void setDConnectionDao(DConnectionDao dConnectionDao) {
